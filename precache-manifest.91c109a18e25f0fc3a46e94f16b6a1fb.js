bf8999c212ac7c64154f7edf910ce9c3b1c33077
Benedikt89 <constantine.kostka@gmail.com>
2020-01-20 04:03:44 +0300

    Updates

---
+++ precache-manifest.91c109a18e25f0fc3a46e94f16b6a1fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb664dbd91c41ba524ba5432ff806f6",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d68d6f63842c18c1b05d",
+    "url": "/tic-tac-toe/static/css/main.87d1015c.chunk.css"
+  },
+  {
+    "revision": "3e736133dbb0e666f46e",
+    "url": "/tic-tac-toe/static/js/2.6f7b84c4.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/tic-tac-toe/static/js/2.6f7b84c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d68d6f63842c18c1b05d",
+    "url": "/tic-tac-toe/static/js/main.19715630.chunk.js"
+  },
+  {
+    "revision": "48aa8ad61f6d3430d724",
+    "url": "/tic-tac-toe/static/js/runtime-main.eb91ba0c.js"
+  },
+  {
+    "revision": "3e6608c53299d6bb5ba946533fc880f7",
+    "url": "/tic-tac-toe/static/media/Spinner.3e6608c5.svg"
+  },
+  {
+    "revision": "184453072f4d631c731fde9092266d17",
+    "url": "/tic-tac-toe/static/media/cross.18445307.svg"
+  },
+  {
+    "revision": "03f0be9ab705360358e0a3b20032a2f0",
+    "url": "/tic-tac-toe/static/media/zero.03f0be9a.svg"
+  }
+]);
